5e19f5064d9cf7c8a5ffba05c7046012895ead4a
smanalorkecil <smanalorkecil2013@gmail.com>
2026-09-19 08:26:15 +0800

    Tambah sitemap dan robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: "https://smanalorkecil.web.id", // Ganti dengan alamat domain resmi websitemu
+  integrations: [tailwind(), sitemap()],
 });
