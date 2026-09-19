7a075c4a7f04674804560ed4e395e42846f02324
smanalorkecil <smanalorkecil2013@gmail.com>
2026-09-19 08:37:33 +0800

    Pasang @astrojs/tailwind resmi dan perbaiki import CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://smanalorkecil.web.id", // Pastikan sesuai alamat domain sekolah
-  integrations: [sitemap()],
+  site: "https://smanalorkecil.web.id",
+  integrations: [tailwind(), sitemap()],
 });
