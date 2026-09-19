1f60d3dbe67871acd6c3d63169f567b3ad23e734
smanalorkecil <smanalorkecil2013@gmail.com>
2026-09-19 08:30:08 +0800

    Perbaiki konfigurasi astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://smanalorkecil.web.id", // Ganti dengan alamat domain resmi websitemu
-  integrations: [tailwind(), sitemap()],
+  site: "https://smanalorkecil.web.id", // Pastikan sesuai alamat domain sekolah
+  integrations: [sitemap()],
 });
